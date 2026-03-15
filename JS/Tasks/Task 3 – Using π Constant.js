59d16298e69926ccc5aed8daf80d2a25cd0a56b5
SaeedALBatta <saeed_samer@outlook.com>
2026-03-15 11:35:47 +0300

    Uploade Problem solve

--- JS/Tasks/Task 3 – Using π Constant.js
+++
@@ -1,8 +0,0 @@
-// Calculate the area of a circle
-
-const PI = 3.14159265359;
-let Raduis = 20; 
-let AreaOfCircle = PI * Math.pow(Raduis , 2)
-console.log(AreaOfCircle);
-
-//
--- JS/Tasks/Task 7 – Even or Odd Number copy.js
+++
@@ -1,9 +0,0 @@
-let Num =prompt("Please Enter Number?");
-
-Num = Number(Num);
-if(Num % 2 === 0){
-    window.alert(`The Number ${Num} Is Even.`);
-}
-else
-    window.alert(`The Number ${Num} Is Odd.`);
-    
